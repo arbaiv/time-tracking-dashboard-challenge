1a2c9a0093a72d3a6e783ba5e5045ad2f3dbd450
Abdur Rahaman <arboni2459@gmail.com>
2021-12-22 00:55:44 +0600

    Project complete
    Project completed

--- script/main.js
+++ script/main.js
@@ -1,18 +1,89 @@
 var activityBox = document.querySelectorAll(".activity-box");
 var activityTitle = document.querySelectorAll(".activity-title");
-var currentTime = document.querySelectorAll(".current-time");
-var presentTime = document.querySelectorAll(".present-time");
+var currentTimeEl = document.querySelectorAll(".current-time");
+var previousTimeEl = document.querySelectorAll(".previous-time");
+var navbar = document.querySelector("#navbar ul");
+var navLinks = document.querySelectorAll("#navbar ul a");
 
-var daily = document.getElementById("daily");
-var weekly = document.getElementById("weekly");
-var monthly = document.getElementById("monthly");
-
-
-(function(){
-    daily.className = "active";
-    $.getJSON("data/data.json", function(data){
-        for(var i = 0; i < activityBox.length; i++){
-            activityTitle[i].textContent = data[0].title;
+(function (){
+    for(var j = 0; j < navLinks.length; j++){
+        navLinks[j].className = "";
+    }
+    navLinks[0].className = "active";
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            for(var i = 0; i < activityBox.length; i++){
+                var data = JSON.parse(xhr.responseText);
+                var title = data[i].title;
+                var currentTime = data[i].timeframes.daily.current;
+                var previousTime = data[i].timeframes.daily.previous;
+                activityTitle[i].textContent = title;
+                currentTimeEl[i].textContent = currentTime + "Hrs";
+                previousTimeEl[i].textContent = "Yesterday - " + previousTime + "Hrs";
+            }
         }
-    });
+    };
+    xhr.open("GET", "../data/data.json", true);
+    xhr.send(null);
 }());
+
+navbar.addEventListener("click", function(e){
+    loadTimeframe(e);
+});
+function loadTimeframe(e){
+    if(e.target.hasAttribute("href")){
+        e.preventDefault();
+        var activeTime = e.target.id;
+        for(var j = 0; j < navLinks.length; j++){
+            navLinks[j].className = "";
+        }
+        e.target.className = "active";
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function(){
+            if(xhr.status === 200){
+                var data = JSON.parse(xhr.responseText);
+                var title, currentTime, previousTime;
+                for(var i = 0; i < activityBox.length; i++){
+                    switch(activeTime){
+                        case "daily":
+                            title = data[i].title;
+                            currentTime = data[i].timeframes.daily.current;
+                            previousTime = data[i].timeframes.daily.previous;
+                            activityTitle[i].textContent = title;
+                            currentTimeEl[i].textContent = currentTime + "Hrs";
+                            previousTimeEl[i].textContent = "Yesterday - " + previousTime + "Hrs";
+                            break;
+                        case "weekly":
+                            title = data[i].title;
+                            currentTime = data[i].timeframes.weekly.current;
+                            previousTime = data[i].timeframes.weekly.previous;
+                            activityTitle[i].textContent = title;
+                            currentTimeEl[i].textContent = currentTime + "Hrs";
+                            previousTimeEl[i].textContent = "Yesterday - " + previousTime + "Hrs";
+                            break; 
+                        case "monthly":
+                            title = data[i].title;
+                            currentTime = data[i].timeframes.monthly.current;
+                            previousTime = data[i].timeframes.monthly.previous;
+                            activityTitle[i].textContent = title;
+                            currentTimeEl[i].textContent = currentTime + "Hrs";
+                            previousTimeEl[i].textContent = "Yesterday - " + previousTime + "Hrs";
+                            break;     
+                        default: 
+                            title = data[i].title;
+                            currentTime = data[i].timeframes.daily.current;
+                            previousTime = data[i].timeframes.daily.previous;
+                            activityTitle[i].textContent = title;
+                            currentTimeEl[i].textContent = currentTime + "Hrs";
+                            previousTimeEl[i].textContent = "Yesterday - " + previousTime + "Hrs";
+                            break;  
+                    }
+
+                }
+            }
+        };
+        xhr.open("GET", "../data/data.json", true);
+        xhr.send(null);
+    }
+}
